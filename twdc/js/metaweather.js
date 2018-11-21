aaefcaf46c2ef4b5fb1f8a1af98db4332ffb9c5b
Gabriel Hart <gabe@ghart.com>
2018-11-21 16:10:12 -0600

    mw corrections

--- twdc/js/metaweather.js
+++ twdc/js/metaweather.js
@@ -1,63 +1,60 @@
 (function() {
     // Create the connector object
     var myConnector = tableau.makeConnector();
- 
+
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         // define columns in the table
         var cols = [{
             id: "applicable_date",
-            dataType: tableau.dataTypeEnum.date,
-            alias: "Date"
-        }, {
-            id: "weather_state_name",
-            dataType: tableau.dataTypeEnum.string,
-            alias: "Weather State"
+            alias: "Date",
+            dataType: tableau.dataTypeEnum.date
         }, {
             id: "min_temp",
-            dataType: tableau.dataTypeEnum.float,
-            alias: "Min Temp"
+            alias: "Min Temp",
+            dataType: tableau.dataTypeEnum.float
         }, {
             id: "max_temp",
-            dataType: tableau.dataTypeEnum.float,
-            alias: "Max Temp"
+            alias: "Max Temp",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "weather_state_name",
+            alias: "Weather Conditions",
+            dataType: tableau.dataTypeEnum.string
         }];
- 
+
         var tableSchema = {
-            id: "metaweather",
-            alias: "MetaWeather - Chicago IL",
+            id: "metaWeather",
+            alias: "MetaWeather data for Chicago IL",
             columns: cols
         };
- 
+
         schemaCallback([tableSchema]);
     };
- 
+
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-         
-        // Change the url in the getJSON() function to point at your API
-        $.getJSON("https://metaweather.com/api/location/2379574/", function(resp) {
+        $.getJSON("https://www.metaweather.com/api/location/2379574/", function(resp) {
             var feat = resp.consolidated_weather,
                 tableData = [];
- 
-            // Update getData() function to iterate through your API response array
-            for (var i = 0, len = feat.length; i < len; i++) {  
-                // Update getData() function with correct field names
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "applicable_date": feat[i].applicable_date, 
-                    "weather_state_name": feat[i].weather_state_name, 
-                    "min_temp": feat[i].min_temp, 
-                    "max_temp": feat[i].max_temp
+                    "applicable_date": feat[i].applicable_date,
+                    "min_temp": feat[i].min_temp,
+                    "max_temp": feat[i].max_temp,
+                    "weather_state_name": feat[i].weather_state_name
                 });
             }
- 
+
             table.appendRows(tableData);
             doneCallback();
         });
     };
- 
+
     tableau.registerConnector(myConnector);
- 
+
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
